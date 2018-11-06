7b9696a67bd230339afc462095ceffabcd6a0220
Santeri <santeri.j.kaariainen@gmail.com>
2018-11-06 16:25:55 +0200

    unnecessary logging removed

--- socket/index.js
+++ socket/index.js
@@ -2,7 +2,6 @@ var io = require('socket.io')(35639);
 var fetch = require("node-fetch");
 let broadcastTimeout = setInterval(broadcast, 1000);
 io.on('connection', function(socket){
-    console.log("client connected");
     socket.on("accessory", function (data) {
         let fetchURL = "http://localhost:5000/ds?target=" + data.target + "&";
         if (data.mode == "bri" && data.value == -1) {
@@ -11,9 +10,8 @@ io.on('connection', function(socket){
             fetchURL += "on=1&";
         }
         fetchURL += data.mode + "=" + data.value;
-        console.log(data.target + " in progress");
         fetch(fetchURL).then(res => {
-            console.log(data.target + " OK");
+            //done
         });
     });
 });
@@ -21,6 +19,5 @@ io.on('connection', function(socket){
 function broadcast() {
     fetch("http://localhost:5000/ds").then((resp) => resp.json(), (err) => console.log(err)).then(json => {
         io.emit("status", json);
-        // broadcastTimeout = setTimeout(broadcast, 1000);
     });
 }
