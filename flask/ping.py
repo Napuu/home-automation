ed66ea2dfd691e2b5d9c4bb5707e21ab686eaaea
Santeri <santeri.j.kaariainen@gmail.com>
2019-07-11 22:21:40 +0300

    ip fix ':D'

--- flask/ping.py
+++ flask/ping.py
@@ -2,7 +2,7 @@ import subprocess
 
 def isLaptopUp():
 
-    process = subprocess.check_output("ping 192.168.1.253 -t 1 -c 3; exit 0", shell=True)
+    process = subprocess.check_output("ping 192.168.1.26 -t 1 -c 3; exit 0", shell=True)
     if " 0 received," in process.decode("utf-8"):
         return False 
     else:
