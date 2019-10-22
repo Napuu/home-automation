c4711054390af77e3c4dfc7bc47dbb8e6e96d7d5
Santeri <santeri.j.kaariainen@gmail.com>
2019-10-22 11:41:47 +0300

    now handling timeouts

--- laptop-scripts/screenscript.sh
+++ laptop-scripts/screenscript.sh
@@ -1,13 +1,43 @@
 #!/bin/bash
+okcounter=0
 while :
 do
-  status=$(curl "http://192.168.1.122:5000/ds?target=0" -s)
-  if [[ $status = *"1"* ]]; then
-    echo "status on"
-    echo "1000" | sudo tee /sys/class/backlight/intel_backlight/brightness
+  status=$(curl "http://192.168.1.122:5000/ds?target=9" -s)
+
+  if [[ $okcounter -lt 1 ]]; then
+    xset -display :0.0 dpms force off
   else
-    echo "status off"
-    echo "0" | sudo tee /sys/class/backlight/intel_backlight/brightness
+    xset -display :0.0 dpms force on
   fi
-	sleep 5
+
+  if [[ $status = *"1"* ]]; then
+    if [[ $okcounter -lt 3 ]]; then
+      echo "increment"
+      ((okcounter=okcounter+1))
+    fi
+  else
+    if [[ $okcounter -gt 0 ]]; then
+      echo "decrement"
+      ((okcounter=okcounter-1))
+    fi
+  fi
+	sleep 2
+
+
+
+
+  #if [[ $status = *"1"* ]]; then
+    #if [[ $okcounter -gt 3 ]]; then
+      #echo "status on"
+      #xset -display :0.0 dpms force on 
+    #else
+      #echo "lastok ok"
+      #((lastok=lastok+1))
+    #fi
+  #else
+    #echo "status off"
+    #lastok="no"
+    #xset -display :0.0 dpms force off 
+  #fi
+	#sleep 2
 done
